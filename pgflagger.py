e35ad651aaddc76c37bed8faa269e4e93b214c74
Bradley Frank <frank.brad@gmail.com>
2014-02-04 23:50:46 +0100

    Updated pgflagger.py so that we don't have to copy mirexecb.py to the working directory. Added watch_process.py, which uses psutil to collect and plot some very simple statistics about a running process. Note that a command.txt sort of file, has to be used and specified for the script to run. Each line should contain the command line incantation of the script, e.g: bash hello_world.sh python hello_world.py -c whatever -b more,stuff

--- pgflagger.py
+++ pgflagger.py
@@ -1,7 +1,7 @@
 import sys
 import miriad
 import mirexec
-import mirexecb
+from apercal import mirexecb
 import aplpy
 import pylab as pl
 import numpy
@@ -26,7 +26,7 @@ parser.add_option('--log', '-l', type='string', dest='log', default=None,
 parser.add_option('--stokes', type='string', dest='stokes', default='qq', 
 	help='Stokes to operate on. [qq]');
 parser.add_option('--flagpar', '--f', type='string', dest='flagpar', 
-	default='3,5,10,5,5,3', help='Flagpar for SumThresholding [3,5,10,5,5,3]');
+	default='5,2,2,3,5,3', help='Flagpar for SumThresholding [5,2,2,3,5,3]');
 parser.add_option('--options', '-o', type='string', dest='options', 
 	default='nodisp', help = 'PGFLAG options [nodisp].'); 
 
