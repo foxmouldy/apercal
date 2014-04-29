cff7834bb5d49eb34751020d3a677e5572b0185a
Bradley Frank <frank.brad@gmail.com>
2014-04-29 23:38:46 +0200

    Updated the timing and reporting functionality.

--- sp_tests/run_miriad_tests.sh
+++ sp_tests/run_miriad_tests.sh
@@ -1,26 +1,29 @@
 sleep 5
-time python swatch_family.py --ptype start --cfile invert.sh --totaltime 10000 --tag restor 
+echo MIRIAD Tests>> report.txt
+echo >> report.txt
+echo invert >> report.txt
+( time python swatch_family.py --ptype start --cfile invert.sh --totaltime 10000 --tag restor 
 sleep 5
 echo clean_n10 >> report.txt
-time python swatch_family.py --ptype start --cfile clean_n10.sh --totaltime 10000 --tag miriad_clean.n10 >> report.txt
+( time python swatch_family.py --ptype start --cfile clean_n10.sh --totaltime 10000 --tag miriad_clean.n10 ) 2>> report.txt
 sleep 5
 echo restor_n10 >> report.txt
-time python swatch_family.py --ptype start --cfile restor_n10.sh --totaltime 10000 --tag miriad_restor.n10 >> report.txt
+( time python swatch_family.py --ptype start --cfile restor_n10.sh --totaltime 10000 --tag miriad_restor.n10 ) 2>> report.txt
 sleep 5
 echo clean_n100 >> report.txt
-time python swatch_family.py --ptype start --cfile clean_n100.sh --totaltime 10000 --tag miriad_clean.n100 >> report.txt
+( time python swatch_family.py --ptype start --cfile clean_n100.sh --totaltime 10000 --tag miriad_clean.n100 ) 2>> report.txt
 sleep 5
 echo restor_n100 >> report.txt
-time python swatch_family.py --ptype start --cfile restor_n100.sh --totaltime 10000 --tag miriad_restor.n100 >> report.txt
+( time python swatch_family.py --ptype start --cfile restor_n100.sh --totaltime 10000 --tag miriad_restor.n100 ) 2>> report.txt
 sleep 5
 echo clean_n1000 >> report.txt
-time python swatch_family.py --ptype start --cfile clean_n1000.sh --totaltime 10000 --tag miriad_clean.n1000 >> report.txt
+( time python swatch_family.py --ptype start --cfile clean_n1000.sh --totaltime 10000 --tag miriad_clean.n1000 ) 2>> report.txt
 sleep 5
 echo restor_n1000 >> report.txt
-time python swatch_family.py --ptype start --cfile restor_n1000.sh --totaltime 10000 --tag miriad_restor.n1000 >> report.txt
+( time python swatch_family.py --ptype start --cfile restor_n1000.sh --totaltime 10000 --tag miriad_restor.n1000 ) 2>> report.txt
 sleep 5
 echo clean_n10000 >> report.txt
-time python swatch_family.py --ptype start --cfile clean_n10000.sh --totaltime 10000 --tag miriad_clean.n10000 >> report.txt
+( time python swatch_family.py --ptype start --cfile clean_n10000.sh --totaltime 10000 --tag miriad_clean.n10000 ) 2>> report.txt
 sleep 5
 echo restor_n10000 >> report.txt
-time python swatch_family.py --ptype start --cfile restor_n10000.sh --totaltime 10000 --tag miriad_restor.n10000 >> report.txt
+( time python swatch_family.py --ptype start --cfile restor_n10000.sh --totaltime 10000 --tag miriad_restor.n10000 ) 2>> report.txt
