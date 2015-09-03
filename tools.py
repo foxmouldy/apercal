dbc962f6cd265cd3fb9cd5bd6c8f66ef63629abb
Bradley Frank <frank.brad@gmail.com>
2015-09-03 10:19:14 +0200

    Updated tools.py

--- tools.py
+++ tools.py
@@ -12,10 +12,15 @@ def invertr(settings):
     Function to do invert on a visibility file.
     '''
     # Get the relevant settings
+    print "Getting the IMAGE settings"
     params = settings.get('image')
+    # Get the vis file from the data settings:
+    vis = settings.get('data', 'vis')
     
     # Move to the correct directory
     full_path = settings.full_path()
+    print "Operating in "+full_path
+
     if not os.path.exists(full_path):
         o, e = shrun('mkdir '+full_path)
         print o, e
@@ -29,20 +34,39 @@ def invertr(settings):
     shrun('rm -r beam')
     
     # Run the taskl
-    O, E = mirrun(task='invert', vis = '../'+params.vis, line=params.line, 
+    O, E = mirrun(task='invert', vis = '../'+vis, line=params.line, 
                select=params.select, slop=params.slop, map = 'map', beam = 'beam', 
                options = params.options, stokes=params.stokes, imsize=params.imsize,
                cell = params.cell, robust=params.robust)
     print O, E
 
+def exceptioner(O, E):
+    '''
+    exceptioner(O, E) where O and E are the stdout outputs and errors. 
+    A simple and stupid way to do exception handling. 
+    '''
+    if len(E)>0:
+        print O
+        print "\n"
+        print "Possible Error"
+        print E
+    else:
+        print "Seems to have ended normally"
+        print O+"\n"
+        print E
+
 def clean(settings):
     '''
+    Module to clean a visibility set. 
     '''
     # Get the relevant settings
+    print "Getting the IMAGE settings"
+
     params = settings.get('image')
     
     # Move to the correct directory
     full_path = settings.full_path()
+    print "Operating in "+full_path
     if not os.path.exists(full_path):
         o, e = shrun('mkdir '+full_path)
         print o, e
@@ -62,8 +86,8 @@ def clean(settings):
         O, E = mirrun(task='clean', map = 'map', beam = 'beam', niters='10000', 
                       out = 'model', cutoff = params.cutoff, region='"mask(mask)"')
 
-    # Print the output    
-    print O, E                    
+    # Exception handling, Print the output    
+    exceptioner(O, E)
     
     # Cleanup
     shrun('rm -r image')
@@ -75,17 +99,18 @@ def clean(settings):
     O, E = mirrun(task='restor', map = 'map', beam = 'beam', 
                   model = 'model', out='residual', mode = 'residual') 
     # Print the output
-    print O, E    
-
+    exceptioner(O, E)    
     
 def maths(settings):
     '''
     Module to make the mask. 
     '''
+    print "Getting the IMAGE parameters"
     params = settings.get('image')
     
     full_path = settings.full_path()
-    
+    print "Operating in "+full_path
+
     if not os.path.exists(full_path):
         o, e = shrun('mkdir '+full_path)
         print o, e
@@ -94,19 +119,11 @@ def maths(settings):
     else:
         os.chdir(full_path)
     
+    print "Removing old mask..."
     shrun('rm -r mask')
     
     O, E = mirrun(task='maths', exp='image', mask='image.gt.'+str(params.mcutoff), out='mask')
-    print O, E
-    
-    #maths = mirexec.TaskMaths()
-    #maths.exp = '<'+image+'>'
-    #maths.mask = '<'+image+'>'+".gt."+str(cutoff)
-    #maths.out = mask
-    #tout = maths.snarf()
-    
-    
-    
+    exceptioner(O, E)
     
 def get_params(config_parser, section):
     params = Bunch()
@@ -134,6 +151,34 @@ def mirrun(task=None, verbose=False, **kwargs):
     else:
         print "Usage = mirrun(task='sometask', arg1=val1, arg2=val2...)"
 
+def selfcal(settings):
+    '''
+    Module to run selfcal
+    '''
+    
+    print "Getting the SELFCAL parameters"
+    params = settings.get('selfcal')
+    
+    vis = settings.get('data', 'vis')
+    full_path = settings.full_path()
+    
+    print "Doing SELFCAL on: "+vis
+    
+    print "Operating in "+full_path
+    if not os.path.exists(full_path):
+        o, e = shrun('mkdir '+full_path)
+        print o, e
+        os.chdir(full_path)
+        print 'making path'
+    else:
+        os.chdir(full_path)
+        
+    O, E = mirrun(task='selfcal', vis='../'+vis, model='model',
+                  select='"'+params.select+'"', interval=params.interval, 
+                  clip = params.clip, options=params.options, minants=params.minants, 
+                  refant = params.refant, line=params.line)
+    exceptioner(O, E)
+    
 def shrun(cmd):
 	'''
 	shrun: shell run - helper function to run commands on the shell.
@@ -144,3 +189,39 @@ def shrun(cmd):
 	out, err = proc.communicate()
 	# NOTE: Returns the STD output.
 	return out, err
+
+def ms2uvfits(inms=None):
+    '''
+    ms2uvfits(inms=None)
+    Utility to convert inms to a uvfile
+    '''
+    outuvf = inms.replace(".MS", ".UVF")
+    cmd = "~/ms2uvfits ms="+inms+" fitsfile="+outuvf+" writesyscal=T multisource=T combinespw=T"
+    print cmd
+    #print "Converted ", outuvf
+    o, e =shrun(cmd)
+    print o, e
+
+def wsrtfits(uvf, uv=None):
+	# NOTE: Import the fits file
+	if uv==None:
+		uv = uvf.replace('.UVF','.UV')
+	cmd = 'wsrtfits in='+uvf+' op=uvin velocity=optbary out='+uv
+	shrun(cmd)
+	# NOTE: Tsys Calibration
+	shrun("attsys vis="+uv+" out=temp")
+	shrun('rm -r '+uv)
+	shrun('mv temp '+uv);
+	shrun('rm -r temp')
+
+def flag(settings):
+	'''
+	UVFLAG: Gets variables from settings.get('flag'). Flags settings.get('flag', 'flags') on 
+	visibilities settings.get('flag', 'vis')
+	'''
+	vis = settings.get('flag', 'vis')
+	flags = settings.get('flag', 'flags')
+	for v in vis.split(','):
+		for f in flags:
+			o, e = mirrun(task='uvflag', vis=v, select=f, flagval='flag')
+			exceptioner(o, e)
