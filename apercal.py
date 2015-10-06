4109285b3cd657792584198c71450f200e8dbcdc
Bradley Frank <frank.brad@gmail.com>
2015-10-06 11:11:30 +0200

    Cleaned up the branch, and removed all the unnecessary files.

--- apercal.py
+++ apercal.py
@@ -1,28 +1,4 @@
-import imp
-try:
-    imp.find_module('mirexecb')
-    found = True
-    import mirexecb
-except ImportError:
-    found = False
-try:
-    imp.find_module('mirexec')
-    found = True
-    import mirexec
-except ImportError:
-    found = False
-import acos
-import acim
-import plot
-import mirplot
-import selfcal
-from ConfigParser import SafeConfigParser
-import subprocess
-import mynormalize
 import pylab as pl
 import lib
 import os
-import tools
-import crosscal
-import image
-import xcal
+import calibrate
